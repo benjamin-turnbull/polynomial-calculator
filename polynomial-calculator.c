af9920da58ac7e6950edcb3fa82d9703f6ebfa87
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-11 14:14:15 +1030

    modified evaluation of polynomial using Horner's method

--- polynomial-calculator.c
+++ polynomial-calculator.c
@@ -9,7 +9,8 @@ int main(void)
     printf("Polynomial: 3x^5+2x^4-5x^3-x^2+7x-6 \nEnter a value for x:\n");
     scanf("%d", &x);
 
-    solution = (3.0 * x * x * x * x * x) + (2.0 * x * x * x * x) - (5.0 * x * x * x) - (x * x) + (7.0 * x) -6.0;
+    /* solution = (3.0 * x * x * x * x * x) + (2.0 * x * x * x * x) - (5.0 * x * x * x) - (x * x) + (7.0 * x) -6.0; */
+    solution = ((((3.0 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6; /* Horner's Method */
 
     printf("Solution: %d", solution);
 }
